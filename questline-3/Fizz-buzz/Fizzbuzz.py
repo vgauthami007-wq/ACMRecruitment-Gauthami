5ef7c1b10a0b42024f65736122d9a138cc8f0e03
vgauthami007-wq <vgauthami007@gmail.com>
2025-09-14 10:22:05 +0530

    Create Fizzbuzz.py

---
+++ questline-3/Fizz-buzz/Fizzbuzz.py
@@ -0,0 +1,16 @@
+# Questline-3: FizzBuzz
+
+def fizzbuzz(n=50):
+    """Print FizzBuzz from 1 to n."""
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizzbuzz()
