19f7c5131b6c234831ee9e33e602ab4d4368a744
vgauthami007-wq <vgauthami007@gmail.com>
2025-09-16 14:32:03 +0530

    Update solution.py

--- questline-3/Eulers_task/solution.py
+++ questline-3/Eulers_task/solution.py
@@ -3,22 +3,20 @@
 # Problem 1
 # Multiples of 3 or 5
 
-def problem1(limit=1000):
-    """Return the sum of all multiples of 3 or 5 below limit."""
-    return sum(i for i in range(limit) if i % 3 == 0 or i % 5 == 0)
+for i in range(1000):
+    if i%3==0 or i%5==0:
+      total+=i
+print(total)
 
 # Problem 2
 # Even Fibonacci Number
-def problem2(limit=4000000):
-    """Return the sum of even Fibonacci numbers below limit."""
-    a, b = 1, 2
-    total = 0
-    while b < limit:
-        if b % 2 == 0:
-            total += b
-        a, b = b, a + b
-    return total
-  
-if __name__ == "__main__":
-    print("Problem 1 Answer:", problem1())
-    print("Problem 2 Answer:", problem2())
+
+a, b = 1, 2   # first two Fibonacci numbers
+total = 0
+
+while b <= 4000000:
+    if b % 2 == 0:          
+        total += b           
+    a, b = b, a + b          
+
+print(total)   # 4613732
