953a88152a4dc613190fba1248c801b8b51d14c0
vgauthami007-wq <vgauthami007@gmail.com>
2025-09-18 22:26:30 +0530

    Update Fizzbuzz.py

--- questline-3/Fizz-buzz/Fizzbuzz.py
+++ questline-3/Fizz-buzz/Fizzbuzz.py
@@ -1,16 +1,11 @@
 # Questline-3: FizzBuzz
 
-def fizzbuzz(n=50):
-    """Print FizzBuzz from 1 to n."""
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
-
-if __name__ == "__main__":
-    fizzbuzz()
+for i in range(1,51):
+    if i%3==0 and i%5==0:
+        print('fizzbuzz')
+    elif i%3==0:
+        print('fizz')
+    elif i%5==0:
+        print('buzz')
+    else:
+        print(i)
