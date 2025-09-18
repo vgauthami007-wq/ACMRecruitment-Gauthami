c6d6e301b9406c1cd35ba2f51563ae3b6dd02ae0
vgauthami007-wq <vgauthami007@gmail.com>
2025-09-18 22:40:05 +0530

    Update rotate_binary.py

--- questline-3/mini-puzzle/rotate_binary.py
+++ questline-3/mini-puzzle/rotate_binary.py
@@ -1,20 +1,16 @@
-def rotate_binary(binary_str: str, k: int) -> int:
-    """
-    Rotate a binary string k steps to the right,
-    then return its decimal value.
-    """
-    n = len(binary_str)
-    k = k % n  # Ensure k is within length
-    rotated = binary_str[-k:] + binary_str[:-k]
-    return int(rotated, 2)
+def rotatebinary(b,k):
+    k=k%(len(b))
+    final=b[-k:]+b[:-k]
+    return int(final,2)
 
-if __name__ == "__main__":
-    b = "1011"   # Binary for 11
-    k = 2
-    print(f"Original binary: {b}")
-    print(f"Rotated {k} steps -> Decimal:", rotate_binary(b, k))
 
-    b = "11001"  # Binary for 25
-    k = 3
-    print(f"\nOriginal binary: {b}")
-    print(f"Rotated {k} steps -> Decimal:", rotate_binary(b, k))
+
+#usage example
+binary='1011'
+k=2
+print(rotatebinary(binary,k))
+
+#example2
+binary='101001'
+k=8
+print(rotatebinary(binary,k))
