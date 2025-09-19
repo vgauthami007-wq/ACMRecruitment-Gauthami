9240e804a3d6ae14d4d5d02e0e2c3b4caa67591e
vgauthami007-wq <vgauthami007@gmail.com>
2025-09-19 17:17:46 +0530

    Update string_reversal.py

--- questline-6/string_riversal/string_reversal.py
+++ questline-6/string_riversal/string_reversal.py
@@ -1,19 +1,18 @@
-# Iterative Method
-def reverse_iterative(text):
-    reversed_text = ""      
-    for i in range(len(text) - 1, -1, -1):
-        reversed_text += text[i]
-    return reversed_text
+# Iterative method
+def iterative(s):
+    r = ""
+    for i in s:
+        r =i+r 
+    return r
 
 
-# Recursive Method
-def reverse_recursive(text):
-    if len(text) <= 1:
-        return text
-    return text[-1] + reverse_recursive(text[:-1])
+# Recursive method
+def recursive(s):
+    return s[::-1] 
 
-if __name__ == "__main__":
-    word = "Questline"   # you can change this to test with any word
-    print("Original String:", word)
-    print("Reversed (Iterative):", reverse_iterative(word))
-    print("Reversed (Recursive):", reverse_recursive(word))
+
+s = "unibic"
+print("Original string:", s)
+print("Reversed (Iterative):", iterative(s))
+print("Reversed (Recursive):", recursive(s))
+
